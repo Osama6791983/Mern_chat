a0919764a84d21762d132417e016d9cf1df577a7
imrankhan992 <imrankohistani65@gmail.com>
2024-05-01 00:55:48 +0500

    url update again

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:3000"],
+        origin: ["http://localhost:3000","https://quickchat-ci0f.onrender.com"],
         methods: ["GET", "POST"],
         credentials: true
     }
