400f0e967f7f961ca80d7d483bb273ba771c6411
fa20-bse-115 <usamahabib12333@gmail.com>
2024-06-11 14:43:06 +0500

    new code changes

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectToMongoDB = async()=>{
     try {
-        await mongoose.connect(process.env.MONGO_DB_URI)
+        await mongoose.connect("mongodb://localhost:27017/quckchat")
         console.log("Connected to mongodb");
     } catch (error) {
         console.log(`error while connecting ${error.message}`);
